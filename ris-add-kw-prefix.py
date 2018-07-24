76befad8a994dc8252ec18b136db19b60ce5661a
Shuyi Wang <nkwshuyi@gmail.com>
2018-07-24 18:18:58 +0800

    updated regex under python 3

--- ris-add-kw-prefix.py
+++ ris-add-kw-prefix.py
@@ -2,23 +2,24 @@ import re
 import sys
 
 def add_kw_prefix(data):
-    regex = r"^([^\-\s]+?)\s+$"
-    subst = "KW  - \\1"
+    regex = r"^(?!(^\S+\s+-))(.+)$"
+    subst = "KW  - \\2"
     newdata = re.sub(regex, subst, data, 0, re.MULTILINE)
     return newdata
 
 def main(argv):
 
-    if not len(argv) == 2:
-        print "Please input the source RIS filename!"
-    else:
-        with open(argv[1]) as f:
-            data = f.read()
+    # if not len(argv) == 2:
+    #     print "Please input the source RIS filename!"
+    assert len(argv) == 2, "Please input the source RIS filename!" 
+        
+    with open(argv[1], encoding='utf8') as f:
+        data = f.read()
 
-        newdata = add_kw_prefix(data)
-        outfile = "output.ris"
-        with open(outfile, 'w') as f:
-            f.write(newdata)
+    data = add_kw_prefix(data)
+    outfile = "output.ris"
+    with open(outfile, 'w', encoding='utf8') as f:
+        f.write(data)
 
 if __name__ == "__main__":
     main(sys.argv)
